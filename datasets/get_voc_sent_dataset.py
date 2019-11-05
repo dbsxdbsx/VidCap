0952d29397617d6ccc154b7ae3e248370233d8e5
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2019-11-05 17:02:56 +1030

    add pascalvocsent downloader

---
+++ datasets/get_voc_sent_dataset.py
@@ -0,0 +1,91 @@
+"""
+Modified from https://github.com/rupy/PascalSentenceDataset
+Thanks Rupy, nice drum set
+"""
+
+import csv
+import os
+import requests
+
+from pyquery import PyQuery
+from tqdm import tqdm
+from urllib.parse import urljoin
+
+
+DATASET_DIR = os.path.join('datasets', 'PascalVOCSent')
+DATASET_URL = 'http://vision.cs.uiuc.edu/pascal-sentences/'
+
+
+def download_images():
+
+    dom = PyQuery(DATASET_URL)
+    for img in tqdm(dom('img').items(), desc="Downloading images from %s" % DATASET_URL, total=1000):
+        img_src = img.attr['src']
+        category, img_file_name = os.path.split(img_src)
+
+        # make category directories
+        output_dir = os.path.join(DATASET_DIR, 'images', category)
+        os.makedirs(output_dir, exist_ok=True)
+
+        # download image
+        output = os.path.join(output_dir, img_file_name)
+        if img_src.startswith('http'):
+            img_url = img_src
+        else:
+            img_url = urljoin(DATASET_URL, img_src)
+        if os.path.isfile(output):
+            print("Already downloaded, Skipping: %s" % output)
+            continue
+        with open(output, 'wb') as f:
+
+            while True:
+                result = requests.get(img_url)
+                raw = result.content
+                if result.status_code == 200:
+                    f.write(raw)
+                    break
+                print("error occurred while fetching img")
+                print("retry...")
+
+
+def download_sentences():
+    dom = PyQuery(DATASET_URL)
+    # tbody disappears in pyquery DOM
+    for tr in tqdm(dom('body>table>tr').items(), desc="Downloading sentences from %s" % DATASET_URL, total=1000):
+        img_src = tr('img').attr['src']
+        category, img_file_name = os.path.split(img_src)
+
+        # make category directories
+        output_dir = os.path.join(DATASET_DIR, 'sentences', category)
+        os.makedirs(output_dir, exist_ok=True)
+
+        # dowonload sentences
+        head, tail = os.path.splitext(img_file_name)
+        sentence_file_name = head + "txt"
+        output = os.path.join(output_dir, sentence_file_name)
+        if os.path.isfile(output):
+            print("Already downloaded, Skipping: %s" % output)
+            continue
+        with open(output, 'w') as f:
+            for td in tr('table tr td').items():
+                f.write(td.text() + "\n")
+
+
+def create_correspondence_data():
+    output = os.path.join(DATASET_DIR, 'correspondence.csv')
+    dom = PyQuery(DATASET_URL)
+    writer = csv.writer(open(output, 'wb'))
+    for i, img in enumerate(dom('img').items()):
+        img_src = img.attr['src']
+        print("%d => %s" % (i + 1, img_src))
+        writer.writerow([i + 1, img_src])
+
+
+if __name__ == "__main__":
+
+    # download images
+    download_images()
+    # download sentences
+    download_sentences()
+    # create correspondence data by dataset
+    create_correspondence_data()
